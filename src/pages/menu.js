2f91b900eadb4c7590834ac976a50f163e9b6ccd
deeppaz <deeppaz@hotmail.com>
2019-11-10 04:18:35 +0300

    added navs and pages

---
+++ src/pages/menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class menu extends Component {
+    render() {
+        return (
+            <div>
+                <h1>menu sayfasi</h1>
+            </div>
+        )
+    }
+}
+
+export default menu;
