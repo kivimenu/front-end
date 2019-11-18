7b2cae8ccf708d9a23a98dd19c4266d1f449de91
deeppaz <deeppaz@hotmail.com>
2019-11-19 01:05:26 +0300

    update menu card

--- src/data/menu.js
+++ src/data/menu.js
@@ -2,20 +2,20 @@ const posts = [
   {
       "id" : 1,
       "title" : "Sushi",
-      "price":"50,99TL",
+      "price":"41,99TL",
       "image": require("../img/paella.jpg"),
       "description": "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
   },
   {
       "id" : 2,
-      "title" : "Sushi",
-      "price":"50,99TL",
+      "title" : "Steak",
+      "price":"12,92TL",
       "image": "https://material-ui.com/static/images/cards/paella.jpg",
       "description": "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
   },
   {
       "id" : 3,
-      "title" : "Sushi",
+      "title" : "Coke",
       "price":"50,99TL",
       "image": "https://material-ui.com/static/images/cards/paella.jpg",
       "description": "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -12,8 +12,11 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
+
+import PlusOneIcon from '@material-ui/icons/PlusOne';
+import ExposureNeg1Icon from '@material-ui/icons/ExposureNeg1';
 
 //data
 import posts from '../data/menu.js';
@@ -31,8 +34,21 @@ const styles = {
     flexGrow: 1,
   },
   fab: {
-    margin: 1,
+    margin: 10,
+    width: 42,
+    height: 42
   },
+  PriceOfYemek: {
+    padding: 5,
+    width: 100,
+    position: 'absolute',
+    bottom: '30%',
+    left: '65%',
+  },
+  DecInc: {
+    position: 'sticky',
+    left: '65%',
+  }
 };
 
 class menu extends Component {
@@ -47,8 +63,7 @@ class menu extends Component {
     const { posts } = this.state;
     return (
       <div className={classes.root}>
-        <Grid container spacing={1} direction="row" justify="space-around" alignItems="center">
-          <Grid item xs={12}>
+        <Grid container spacing={1} item xs={12} direction="row" justify="space-around" alignItems="center">
             <Card className={classes.card}>
               {
                 posts.map(post => (
@@ -60,6 +75,9 @@ class menu extends Component {
                         <Typography gutterBottom variant="h5" component="h2">
                           {post.title}
                         </Typography>
+                        <Typography className={classes.PriceOfYemek} variant="h5" component="h3">
+                          {post.price}
+                        </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
                           {post.description}
                         </Typography>
@@ -67,11 +85,19 @@ class menu extends Component {
                     </CardActionArea>
                     <CardActions>
                       <Fab color="primary" aria-label="add" className={classes.fab}>
-                        <AddIcon />
+                        <AddShoppingCartIcon fontSize="small" />
                       </Fab>
-                      <Fab color="secondary" aria-label="edit" className={classes.fab}>
-                        <EditIcon />
+                      <Fab  aria-label="edit" className={classes.fab}>
+                        <ImportContactsIcon fontSize="small" />
                       </Fab>
+                      <div className={classes.DecInc}>
+                        <Fab color="primary" aria-label="add" className={classes.fab}>
+                          <PlusOneIcon fontSize="small" />
+                        </Fab>
+                        <Fab color="secondary" aria-label="edit" className={classes.fab}>
+                          <ExposureNeg1Icon fontSize="small" />
+                        </Fab>
+                      </div>
                     </CardActions>
                     <br />
                   </div>
@@ -79,7 +105,6 @@ class menu extends Component {
               }
             </Card>
           </Grid>
-        </Grid>
       </div>
     );
   }
