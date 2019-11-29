83655989acad2b99b02d158133decc1ba485e2b6
deeppaz <deeppaz@hotmail.com>
2019-11-29 23:51:47 +0300

    added QR kod dialog

--- src/components/CardQR.js
+++ src/components/CardQR.js
@@ -1,39 +1,44 @@
 import React, { Component } from 'react';
+import QR from '../img/qrkod.jpg'
+
+//mui
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-export class Success extends Component {
-  continue = e => {
-    e.preventDefault();
-    // PROCESS FORM //
-    this.props.nextStep();
-  };
-
-  back = e => {
-    e.preventDefault();
-    this.props.prevStep();
-  };
-
+class CardQR extends Component {
   render() {
     return (
-      <MuiThemeProvider > 
-        <React.Fragment>
-        <Dialog 
-            open={true}
-            fullWidth={true}
-            maxWidth='sm'
-          >
-            <AppBar title="Success" />
-            <h1>Zaman ayırdığınız için teşekkürler</h1>
-            <p>Restoranımıza tekrar geldiğinizde bilgileriniz kayıt olmuş olacak</p>
-            <h3>Menu'ye geçip dilediğinizi seçin. geçmek için <Link to="/menuler">burayı tıklayın</Link></h3>
-          </Dialog>
-        </React.Fragment>
-      </MuiThemeProvider>
+      <React.Fragment>
+        <Dialog
+          open={true}
+          fullWidth={true}
+          maxWidth='sm'
+        >
+          <AppBar title="Success" />
+          <DialogTitle id="alert-dialog-title">{"QR Kodu ile Ödeme Yap"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Alışveriş yaptığınız mağazada ödeme sırasında QR Kod ile ödeme yapacağınızı belirttikten sonra;
+              <br/>
+              a. World Mobil uygulamasında Yapı Kredi Pay butonuna tıkladıktan sonra “QR Kod ile Ödeme”yi seçerek QR Kod okuyucuyu açın ve POS cihazında oluşan QR Kod’u okutun. PAY butonuna uzun basarak da QR Kod okuyucuyu açabilirsiniz.
+              <br/>
+              <img src={QR} width="210" height="210" alt="QR KODU"></img>
+          </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Hayır, Diğer Yolu seçmek istiyorum
+          </Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
     );
   }
 }
 
-export default Success;
+export default CardQR;
